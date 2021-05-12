2fe182c8fe82b034f6067e33a1d88080b15bef08
siridet wongnarat <siridet.wo@rmuti.ac.th>
2021-05-12 15:44:35 +0700

    delete non use

--- src/main/java/rmuti/runnerapp/model/service/FunRunRepository.java
+++
@@ -1,7 +0,0 @@
-package rmuti.runnerapp.model.service;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import rmuti.runnerapp.model.table.FunRun;
-
-public interface FunRunRepository extends JpaRepository<FunRun,Integer> {
-}
--- src/main/java/rmuti/runnerapp/model/service/SaveRunnerRepository.java
+++
@@ -1,7 +0,0 @@
-package rmuti.runnerapp.model.service;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import rmuti.runnerapp.model.table.SaveRunner;
-
-public interface SaveRunnerRepository extends JpaRepository<SaveRunner,Integer> {
-}
